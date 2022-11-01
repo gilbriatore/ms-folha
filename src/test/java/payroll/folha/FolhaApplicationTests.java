d92def745c596f37f748f28dc83415379e7b97b4
Geucimar <geucimar@gmail.com>
2022-11-01 19:34:21 -0300

    Folha

---
+++ src/test/java/payroll/folha/FolhaApplicationTests.java
@@ -0,0 +1,13 @@
+package payroll.folha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
